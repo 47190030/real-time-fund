0e547d7b92562883fb5970facb8731a2e699d99c
47190030 <130479840+47190030@users.noreply.github.com>
2026-03-19 12:51:35 +0800

    更新 PcTableSettingModal.jsx

--- app/components/PcTableSettingModal.jsx
+++ app/components/PcTableSettingModal.jsx
@@ -215,7 +215,7 @@ export default function PcTableSettingModal({
                           )}
                           {item.id === 'relatedSector' && (
                             <span className="muted" style={{ fontSize: '12px' }}>
-                              仅 fund.cc.cd 地址支持
+                              测试阶段
                             </span>
                           )}
                         </div>
