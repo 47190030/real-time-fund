75d5bdbdd2901c4440325b8cf0e2a5f3ab8e2980
47190030 <130479840+47190030@users.noreply.github.com>
2026-03-19 12:50:43 +0800

    更新 MobileSettingModal.jsx

--- app/components/MobileSettingModal.jsx
+++ app/components/MobileSettingModal.jsx
@@ -189,7 +189,7 @@ export default function MobileSettingModal({
                         )}
                         {item.id === 'relatedSector' && (
                           <span className="muted" style={{ fontSize: '12px' }}>
-                            仅 fund.cc.cd 地址支持
+                            测试阶段
                           </span>
                         )}
                       </div>
